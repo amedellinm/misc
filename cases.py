ce563530dc302b270957b288f976a46232b0ae76
Antonio Medellín <amedellinm@outlook.com>
2025-04-20 20:18:11 -0600

    add the otherwise argument to remap and some typehints

--- cases.py
+++ cases.py
@@ -1,15 +1,28 @@
+from collections.abc import Iterable, Mapping, Sequence
+
 from pyspark.sql import functions as F
 from pyspark.sql.column import Column
 
 
-def cases(case_list: list[tuple[Column, Column]], otherwise: Column = F.lit(None)):
+def cases(cases: list[tuple[Column, Column]], otherwise: Column = None) -> Column:
     """Build a chained when-otherwise expression from condition-value pairs."""
     ans = F
-    for condition, value in case_list:
+    for condition, value in cases:
         ans = ans.when(condition, value)
     return ans.otherwise(otherwise)
 
 
-def remap(column, mapping):
+# @TYPEHINTHANDLER
+def remap(
+    column: str | Column,
+    mapping: Mapping | Iterable[Sequence],
+    otherwise: Column = Ellipsis,
+) -> Column:
     """Change the data in `column` from the keys to the values of `mapping`."""
-    return cases([(F.col(column) == key, val) for key, val in mapping.items()])
+    if isinstance(mapping, Mapping):
+        mapping = mapping.items()
+
+    return cases(
+        [(column == key, val) for key, val in mapping],
+        column if otherwise is Ellipsis else otherwise,
+    )
